f40e6ef417c76dd73cf6fae858b82ebe098b49e4
Nina Louw <ninalouw@mac.com>
2017-11-26 20:53:42 -0800

    set up routes, header, simple sign in form

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 
+import Header from './header';
+
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <Header/>
     );
   }
 }
